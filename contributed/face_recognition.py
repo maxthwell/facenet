2afa607ac019bb8a3ac80c836d838a7e227a69e0
ma.xiangxiang <ma.xiangxiang@eisoo.com>
2020-01-19 11:22:42 +0800

    马祥祥: 兼容face_recognition

--- contributed/face_recognition.py
+++ contributed/face_recognition.py
@@ -41,30 +41,55 @@ class FaceRecognition():
     def __init__(self, args):
         self.image_size = args.image_size
         self.margin = args.margin
-        self.pnet, self.rnet, self.onet = create_network_face_detection(args.gpu_memory_fraction)
-        with tf.Graph().as_default(): 
-            with tf.Session() as sess:
-                self.sess = sess
+        self.create_network_face_detection(args.gpu_memory_fraction)
+        graph = tf.Graph()
+        with graph.as_default(): 
+            self.sess = tf.Session()
+            with self.sess.as_default():
                 facenet.load_model(args.model)
-                self.images_placeholder = sess.graph.get_tensor_by_name("input:0")
-                self.embeddings_op = sess.graph.get_tensor_by_name("embeddings:0")
-                self.phase_train_placeholder = sess.graph.get_tensor_by_name("phase_train:0")
+            self.images_placeholder = self.sess.graph.get_tensor_by_name("input:0")
+            self.embeddings_op = self.sess.graph.get_tensor_by_name("embeddings:0")
+            self.phase_train_placeholder = self.sess.graph.get_tensor_by_name("phase_train:0")
 
-    def encoding(self, img): 
+    def create_network_face_detection(self, gpu_memory_fraction):
+        with tf.Graph().as_default():
+            gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
+            sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+            with sess.as_default():
+                self.pnet, self.rnet, self.onet = align.detect_face.create_mtcnn(sess, None)
+
+    def face_encodings(self, img): 
         images = self.align_data(img)
-        if images==None: return None
         feed_dict = {self.images_placeholder: images, self.phase_train_placeholder: False}
-        emb = sess.run(self.embeddings_op, feed_dict=feed_dict)
-        return emb
+        try:
+            emb = self.sess.run(self.embeddings_op, feed_dict=feed_dict)
+        except:
+            return []
+        return [e for e in emb]
 
-    def align_data(self, src_img):
+    def face_distance(self, e1, e2):
+        d = np.sum((e1-e2)**2)**0.5
+        return d 
+
+    def compare_faces(self, encoding_list, encoding, max_distance=1.0):
+        distance_data = np.array([self.face_distance(e,encoding) for e in encoding_list])
+        min_distance = np.min(distance_data)
+        min_distance_idx = np.argmin(distance_data)
+        if min_distance < max_distance: return min_distance_idx
+        else: return -1
+        
+
+    def detect_face(self, src_img):
         minsize = 20  # minimum size of face
         threshold = [0.6, 0.7, 0.7]  # three steps's threshold
         factor = 0.709  # scale factor
-        img_list = []
-
-        img_size = np.asarray(src_img.shape)[0:2]
         bounding_boxes, key_points = align.detect_face.detect_face(src_img, minsize, self.pnet, self.rnet, self.onet, threshold, factor)
+        return bounding_boxes, key_points
+
+    def align_data(self, src_img):
+        img_list = []
+        img_size = np.asarray(src_img.shape)[0:2]
+        bounding_boxes, key_points = self.detect_face(src_img)
         nrof_samples = len(bounding_boxes)
         if nrof_samples > 0:
             for i in range(nrof_samples):
@@ -75,7 +100,7 @@ class FaceRecognition():
                     bb[1] = np.maximum(det[1] - self.margin / 2, 0)
                     bb[2] = np.minimum(det[2] + self.margin / 2, img_size[1])
                     bb[3] = np.minimum(det[3] + self.margin / 2, img_size[0])
-                    cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
+                    cropped = src_img[bb[1]:bb[3], bb[0]:bb[2], :]
                     aligned = cv2.resize(cropped, dsize=(self.image_size, self.image_size))
                     prewhitened = facenet.prewhiten(aligned)
                     img_list.append(prewhitened)
@@ -86,24 +111,9 @@ class FaceRecognition():
             return None
 
 
-def create_network_face_detection(gpu_memory_fraction):
-    with tf.Graph().as_default():
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
-        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
-        with sess.as_default():
-            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
-    return pnet, rnet, onet
-
-
-def load_images_from_folder(folder):
-    images = []
-    imgnames = []
-    for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder, filename))
-        if img is not None:
-            images.append(img)
-            imgnames.append(filename)
-    return images, imgnames
+    def load_image_file(self,imgfile):
+        img = cv2.imread(imgfile)
+        return img
 
 
 def parse_arguments(argv):
@@ -111,10 +121,6 @@ def parse_arguments(argv):
 
     parser.add_argument('model', type=str,
                         help='Either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file')
-    parser.add_argument('data_dir', type=str,
-                        help='The directory containing the images to cluster into folders.')
-    parser.add_argument('out_dir', type=str,
-                        help='The output directory where the image clusters will be saved.')
     parser.add_argument('--image_size', type=int,
                         help='Image size (height, width) in pixels.', default=160)
     parser.add_argument('--margin', type=int,
@@ -131,5 +137,40 @@ def parse_arguments(argv):
     return parser.parse_args(argv)
 
 
-if __name__ == '__main__':
-    fr = FaceRecognition(parse_arguments(sys.argv[1:]))
+fr = FaceRecognition(parse_arguments(sys.argv[1:]))
+
+def load_known_img(known_dir='/data/face/known'):
+    encoding_list = []
+    label_list = []
+    for label in os.listdir(known_dir):
+        sum_encoding = np.zeros([512],dtype=np.float32)
+        num = 0.0
+        for img in os.listdir('%s/%s'%(known_dir,label)):
+            img = fr.load_image_file("%s/%s/%s"%(known_dir,label,img))
+            encodings = fr.face_encodings(img)
+            if len(encodings) == 1:
+                sum_encoding += encodings[0]
+                num += 1
+        encoding_list.append(sum_encoding/num)
+        label_list.append(label)
+    return encoding_list, label_list
+
+encoding_list,label_list = load_known_img()
+
+def recognition(img_file):
+    img = fr.load_image_file(img_file)
+    encodings = fr.face_encodings(img)
+    labels=[]
+    for encoding in encodings:
+        idx = fr.compare_faces(encoding_list, encoding, max_distance=1.0)
+        if idx > -1:
+            label = label_list[idx]
+            labels.append(label)
+    return labels
+
+if __name__=='__main__':
+    for img_file in os.listdir('/data/face/unknown'):
+        labels = recognition('/data/face/unknown/%s'%img_file)
+        print(img_file, labels)
+
+
